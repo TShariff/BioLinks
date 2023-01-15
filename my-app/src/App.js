c5b955a9a6e5df6bc00d8399a5361b0fcb0dfc04
Tamjeed Shariff <98565042+Shariff6-school@users.noreply.github.com>
2023-01-14 18:46:47 -0600

    website created

---
+++ my-app/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.png';
+import './App.css';
+import * as React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <img src={logo} className = "profile-pic" width="200px" alt="logo" style={{width: 150, height: 150, borderRadius: 200/ 2}}/>
+        <h1> @tamjeed.s</h1>
+        <form action="https://tamjeedshariff.tech/">
+        <button class="button3">Personal Website</button>
+        </form>
+        <form action="https://www.instagram.com/tamjeed.s/?hl=en">
+        <button class="button">Instagram</button>
+        </form>
+        <form action="https://github.com/TShariff">
+        <button class="button2">Github</button>
+        </form>
+        <form action="mailto:t.tamjeed.shariff@gmail.com">
+        <button class="button2">Email</button>
+        </form>
+
+
+
+        <q>
+          Why use linktree when I can just make it
+        </q>
+      </header>
+      
+    
+    </div>
+  );
+}
+
+export default App;
